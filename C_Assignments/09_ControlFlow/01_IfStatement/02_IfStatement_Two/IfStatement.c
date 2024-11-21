aed33b4cbbf9a2e0ad4d1942d5de8a11d4112193
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2024-11-21 15:38:26 +0530

    Add files via upload

---
+++ C_Assignments/09_ControlFlow/01_IfStatement/02_IfStatement_Two/IfStatement.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // variable declarations
+    int age;
+
+    // code
+    printf("\n\n");
+    printf("Enter age: ");
+    scanf("%d", &age);
+
+    if (age >= 18)
+    {
+        printf("You are eligible for voting!!!\n\n");
+    }
+
+    return (0);
+}
