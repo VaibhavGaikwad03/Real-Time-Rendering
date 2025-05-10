8ca6f197420401d025b9dde5281fe7432c770329
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2025-05-10 20:18:42 +0530

    Delete C_Assignments directory

--- C_Assignments/10_Functions/01_EntryPointFunction/01_Type_01/EntryPointFunction.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h> // stdio.h contains declarations of 'printf()'
-
-// Entry-point function => main() => no return type (void) and no parameters (void), hence no command-line arguments
-
-void main(void)
-{
-    // code
-    printf("Hello, World!!!\n"); // Library function
-}
